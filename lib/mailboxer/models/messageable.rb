044692e704ac92ac750f8b343958e01c2dc529fc
Alex Ung <alexung.ucsb@gmail.com>
2017-01-18 19:58:20 -0800

    send acceptance_request through messages

--- lib/mailboxer/models/messageable.rb
+++ lib/mailboxer/models/messageable.rb
@@ -62,7 +62,7 @@ module Mailboxer
 
       #Sends a messages, starting a new conversation, with the messageable
       #as originator
-      def send_message(recipients, msg_body, subject, rev_request=false, app_sent=false, rent_payment_request=false, acceptance_request=false, sanitize_text=true, attachment=nil, message_timestamp = Time.now)
+      def send_message(recipients, msg_body, subject, rev_request=false, app_sent=false, rent_payment_request=false, sanitize_text=true, attachment=nil, message_timestamp = Time.now)
 
         convo = Mailboxer::ConversationBuilder.new({
           :subject    => subject,
@@ -78,7 +78,6 @@ module Mailboxer
           :review_request => rev_request,
           :application_sent => app_sent,
           :rent_payment_request => rent_payment_request,
-          :acceptance_request => acceptance_request,
           :subject      => subject,
           :attachment   => attachment,
           :created_at   => message_timestamp,
@@ -90,13 +89,14 @@ module Mailboxer
 
       #Basic reply method. USE NOT RECOMENDED.
       #Use reply_to_sender, reply_to_all and reply_to_conversation instead.
-      def reply(conversation, recipients, reply_body, subject=nil, sanitize_text=true, attachment=nil)
+      def reply(conversation, recipients, reply_body, acceptance_request=false, subject=nil, sanitize_text=true, attachment=nil)
         subject = subject || "#{conversation.subject}"
         response = Mailboxer::MessageBuilder.new({
           :sender       => self,
           :conversation => conversation,
           :recipients   => recipients,
           :body         => reply_body,
+          :acceptance_request => acceptance_request,
           :subject      => subject,
           :attachment   => attachment
         }).build
@@ -117,14 +117,14 @@ module Mailboxer
 
       #Replies to all the recipients of the last message in the conversation and untrash any trashed message by messageable
       #if should_untrash is set to true (this is so by default)
-      def reply_to_conversation(conversation, reply_body, subject=nil, should_untrash=true, sanitize_text=true, attachment=nil)
+      def reply_to_conversation(conversation, reply_body, acceptance_request=false, subject=nil, should_untrash=true, sanitize_text=true, attachment=nil)
         #move conversation to inbox if it is currently in the trash and should_untrash parameter is true.
         if should_untrash && mailbox.is_trashed?(conversation)
           mailbox.receipts_for(conversation).untrash
           mailbox.receipts_for(conversation).mark_as_not_deleted
         end
 
-        reply(conversation, conversation.last_message.recipients, reply_body, subject, sanitize_text, attachment)
+        reply(conversation, conversation.last_message.recipients, reply_body, acceptance_request, subject, sanitize_text, attachment)
       end
 
       #Mark the object as read for messageable.
