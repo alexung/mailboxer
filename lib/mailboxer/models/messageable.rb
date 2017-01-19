8d49c5afe7b5b320ccf0e12ab02105f193c1a019
Alex Ung <alexung.ucsb@gmail.com>
2017-01-19 10:45:08 -0800

    acceptance request in message

--- lib/mailboxer/models/messageable.rb
+++ lib/mailboxer/models/messageable.rb
@@ -62,7 +62,7 @@ module Mailboxer
 
       #Sends a messages, starting a new conversation, with the messageable
       #as originator
-      def send_message(recipients, msg_body, subject, rev_request=false, app_sent=false, rent_payment_request=false, sanitize_text=true, attachment=nil, message_timestamp = Time.now)
+      def send_message(recipients, msg_body, subject, rev_request=false, app_sent=false, rent_payment_request=false, acceptance_request=false, sanitize_text=true, attachment=nil, message_timestamp = Time.now)
 
         convo = Mailboxer::ConversationBuilder.new({
           :subject    => subject,
@@ -78,6 +78,7 @@ module Mailboxer
           :review_request => rev_request,
           :application_sent => app_sent,
           :rent_payment_request => rent_payment_request,
+          :acceptance_request => acceptance_request,
           :subject      => subject,
           :attachment   => attachment,
           :created_at   => message_timestamp,
